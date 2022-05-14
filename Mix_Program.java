597a1c9ce002baf9c45f794a1af42a12bda19172
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-05-14 16:02:56 +0530

    Add files via upload

---
+++ Mix_Program.java
@@ -0,0 +1,16 @@
+package Basic_Java;
+
+//public class Mix_Program {
+
+	//public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        
+	  public class Mix_Program{
+		   public static void main(String[]args) {
+			 System.out.println(-5+8*6);
+			 System.out.println((55+9)%9);
+			 System.out.println(20+(-3)*5/9);
+			 System.out.println(5 + 15/3*2 - 8%3);
+			 //System.out.println( )
+		   }
+		   }

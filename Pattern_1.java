35000dfa4d21e39cdc5e33e0e1a66546beb47f7d
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-05-15 15:22:26 +0530

    Add files via upload

---
+++ Pattern_1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Pattern_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int n = 4;
+     int m = 5;
+     //outer look
+     for(int i=1; i<=n; i++) {
+    	 //ineer loop
+    	  for( int j=1; j<=m; j++) {
+    		  System.out.print("*");
+    	  }
+    	  System.out.println(" ");
+     }
+    	  }
+     }

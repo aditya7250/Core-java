fc10bcb2ea06ac8e7a3a2fd0872d0996ae04e4a9
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-05-17 21:31:56 +0530

    Add files via upload

---
+++ Cmd.java
@@ -0,0 +1,12 @@
+package Prime_Question;
+
+public class Cmd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(args[0]);
+		System.out.println(args[1]);
+
+	}
+
+}

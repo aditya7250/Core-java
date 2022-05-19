89fd990a53457e5573808a23aaf01109dd2b4d92
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-05-20 01:27:16 +0530

    Add files via upload

--- age_problem.java
+++ age_problem.java
@@ -1,19 +1,26 @@
-package coding_ninja;
+//package coding_ninja;
 
 //public class age_problem {
 
 //	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-
 import java.util.*;
 class age_problem {
-	public static void main(String args[]) {
-		Scanner sc = new Scanner(System.in);
-		String name = sc.next();
-		int age = sc.nextInt();
-		System.out.println("The name of the person is " + name + " and the age is " +age+".");
-		// Write code here
-		
+	public static void main(String[]args) {
+	void mystery(int a) {
+		for(int k =2; k<=a; k++) {
+			boolean flag = true;
+			for (int j=2;j<=k/2; j++) {
+				if(k%j==0) {
+					flag = false;
+					break;
+				}
+			}
+			 if(flag) {
+				 System.out.print(k + " ");)
+			 }
+		}
 	}
+}
 }

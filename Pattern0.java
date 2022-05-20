2bf990a14f9653fddbc029174044b4609b208370
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-05-21 01:49:02 +0530

    Add files via upload

---
+++ Pattern0.java
@@ -0,0 +1,15 @@
+package lec2;
+
+public class Pattern0 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int n = 5;
+     int i = 1;
+     while(i<=n) {
+    	 System.out.println("*"+"");
+    	 i = i+1;
+     }
+	}
+
+}

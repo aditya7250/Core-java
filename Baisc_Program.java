fa238e879c2e24ab7c6025724589d9ebd1bb0bad
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-05-22 03:19:04 +0530

    Add files via upload

---
+++ Baisc_Program.java
@@ -0,0 +1,18 @@
+package lec1;
+
+public class Baisc_Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int time =12;
+		 if(time<11) {
+			 System.out.println("Good moring");
+		 }else if (time<12) {
+			 System.out.println("Good evening");
+		 }else {
+			 System.out.println("Good night");
+		 }
+
+	}
+
+}

ce053d4abbebf57c6308f228b13efaac263225c5
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-05-22 03:19:44 +0530

    Add files via upload

--- Hello_Program.java
+++ Hello_Program.java
@@ -1,15 +1,10 @@
-package Basic_Java;
-
-//public class Hello_Program {
-
-	//public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-
+package lec1;
 
 public class Hello_Program {
-     public static void main (String[] args)	 {
-    	  System.out.println("Hello\nAditya yadav!");
-    	  
-     }
-     }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.println("Welcome to my first program");
+	}
+
+}

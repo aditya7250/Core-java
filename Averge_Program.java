bd25849028c64a2f2ed10a65caf681af4a6f4bb6
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-05-22 03:18:41 +0530

    Add files via upload

---
+++ Averge_Program.java
@@ -0,0 +1,29 @@
+package lec1;
+
+public class Averge_Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int Marks = 40;
+		if (Marks>=80){
+		  System.out.println("Grade A");
+		}
+		else if (Marks>=70 && Marks<80){
+			System.out.println("Grade B");
+		}
+		else if (Marks>=60 && Marks<70){
+			System.out.println("Grade C");
+		}
+		else if(Marks>=50 && Marks<60) {
+			System.out.println("Pass");
+		}else {
+			System.out.println("Fail");
+		}
+		
+		  
+		
+		
+
+	}
+
+}

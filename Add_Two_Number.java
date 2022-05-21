cb07aacc562b0e21a9406432ce801c524d37d81a
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-05-22 03:18:11 +0530

    Add files via upload

---
+++ Add_Two_Number.java
@@ -0,0 +1,20 @@
+package lec1;
+
+public class Add_Two_Number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int a=10;
+       int b=10;
+       int c= a+b;
+       System.out.println(c);
+       int a1=5;//update
+       int b1=2;
+       //int c1=a1-b1;
+       //int c1=a1*b1;
+       //int c1=a1/b1;
+       int c1=a1%b1;
+       System.out.println(c1);
+	}
+
+}

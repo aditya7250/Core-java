ee35d8143caf78db3e9afb61d6530146a1eada97
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-05-27 00:20:37 +0530

    Add files via upload

---
+++ add_java.java
@@ -0,0 +1,18 @@
+package Eassy_java21;
+import java.util.*;
+
+
+public class add_java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int a,b,c;
+     System.out.println("Enter the number");
+     Scanner sc = new Scanner (System.in);
+     a = sc.nextInt();
+     b = sc.nextInt();
+     c=a+b;
+     System.out.println("The number of addition is :" +c);
+	}
+
+}

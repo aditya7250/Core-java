65f0a6760234f3b7f0651291efd3c71767f5fc0b
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-05-27 00:20:52 +0530

    Add files via upload

---
+++ basic_1.java
@@ -0,0 +1,18 @@
+package Eassy_java21;
+ import java.util.*;
+
+public class basic_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int age = sc.nextInt();
+		if (age>18) {
+			System.out.println("Adult");
+		}
+		else {
+			System.out.println("Not Adult");
+		}
+	}
+
+}

19cce361f3be50ea76d257fb829e71c917e5f4d3
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-05-28 12:46:43 +0530

    Add files via upload

---
+++ condition_java.java
@@ -0,0 +1,20 @@
+package Eassy_java21;
+import java.util.*;
+public class condition_java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  int password ;
+  System.out.println("Enter the password -");
+   Scanner abc = new Scanner (System.in);
+   password = abc.nextInt();
+    if (password ==6969) {
+    	 System.out.println("Name - Aditya yadav");
+    	 System.out.println("Roll no  - 190132013000002");
+    	 System.out.println("branch- IT");
+    	 System.out.println("College- Gniot");
+   	  
+  }
+	}
+
+}

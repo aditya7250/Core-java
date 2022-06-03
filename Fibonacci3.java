c81f82eba6a6257e18b89e7120df599b2b64ffce
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-06-04 00:06:48 +0530

    Add files via upload

---
+++ Fibonacci3.java
@@ -0,0 +1,29 @@
+package JAVAPROGRAM3;
+import java.util.*;
+public class Fibonacci3 {
+
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter number of terms");
+	int n=sc.nextInt();
+	series(n);
+	}
+	
+	static void series(int num)
+	{
+	System.out.println("Fibonacci series is ");
+	int i=0,j=1,next;
+	for(int c=0;c<num;c++)
+	{
+	if(c<=1)
+		next=c;
+	else
+	{
+	next=i+j;
+	i=j;
+	j=next;
+	}
+	System.out.print(next+" ");
+	}
+	}
+}

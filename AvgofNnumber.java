176acf1132e4f532cf5e8d1bd1afde52f68f05c2
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-06-06 00:09:50 +0530

    Add files via upload

---
+++ AvgofNnumber.java
@@ -0,0 +1,29 @@
+package JavaProgram6;
+import java.util.*;
+public class AvgofNnumber {
+
+	public static void main(String[] args) {
+	
+       int n;double res=0;
+	
+       Scanner sc=new Scanner(System.in);
+ 
+       System.out.println("enter how many numbers to cal  avg");
+	   
+       n=sc.nextInt();
+ 
+       int a[]=new int[n];
+ 
+       System.out.println("enter   "+n+"  numbers");
+ 
+       for(int i=0;i<n;i++)
+	   a[i]=sc.nextInt();
+ 
+       for(int i=0;i<n;i++)
+	   res =res+a[i];
+ 	 	    
+        System.out.println("average="+res/n);
+ 
+    }
+ 
+}

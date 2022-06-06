39990e7a9864c44293459348e5957226bf1febf7
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-06-07 00:16:00 +0530

    Add files via upload

---
+++ CompoundInterest.java
@@ -0,0 +1,27 @@
+package JavaProgram7;
+import static java.lang.Math.pow;
+public class CompoundInterest {
+
+	public static void main(String[] args) {
+	     double amount,principle,rate,time,ci,sum;
+                
+             principle=Double.parseDouble(args[0]);
+                 
+             rate=Double.parseDouble(args[1]);
+                  
+             time=Double.parseDouble(args[2]);	
+			
+	     rate=(1+rate/100);
+	
+	     rate=pow(rate,time);
+ 
+	     amount=principle*rate;
+ 
+	     System.out.println("amount="+amount);
+ 
+	     ci=amount-principle;
+ 
+	     System.out.println("compound interest="+ci);
+ 
+	}
+}

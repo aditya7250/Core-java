381047452bc39dc3353cc76d4d8e0c93fcf65612
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-06-08 01:43:50 +0530

    Add files via upload

---
+++ GCD.java
@@ -0,0 +1,30 @@
+package JAVAPRO8;
+import java.util.*;
+public class GCD {
+
+	public static void main(String[] args) {
+	 long n1,n2;
+	 Scanner sc=new Scanner(System.in);
+	 System.out.println("enter  number 1");
+                n1=sc.nextLong();
+             	 System.out.println("enter  number 2");
+                n2=sc.nextLong();
+	 if(n1>0 && n2>0)
+	 {
+	   long result=gcdCal(n1,n2);
+     	   System.out.println("Gcd of two numbers = "+result);
+	 }  
+	else
+	    System.out.println("Please enter numbers greater than zero");
+}
+static long gcdCal(long a,long b)
+  {
+    while (b > 0)
+    {
+        long temp = b;
+        b = a % b; 
+        a = temp;
+    }
+    return a;
+   }
+}

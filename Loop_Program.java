18f76cd4a08ef2135ea40a7c56f6cc18dc971ed5
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-06-13 20:08:48 +0530

    Add files via upload

---
+++ Loop_Program.java
@@ -0,0 +1,16 @@
+package lec1;
+
+public class Loop_Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 1;
+		  while(i<=10) {
+			  System.out.println("Hello World");
+			   i = i+1;
+			
+		}
+
+	}
+
+}

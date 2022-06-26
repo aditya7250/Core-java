cb87b387c2e3bafceeb9435353f3b4971e8b58de
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-06-26 22:29:25 +0530

    Add files via upload

---
+++ SimpleInterstpProgram.java
@@ -0,0 +1,13 @@
+package javaprogram26;
+
+public class SimpleInterstpProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      float p,r,t,si;
+      P=1300,r=15,t=4;
+      si=(p*r*t)/100;
+      System.out.println(+si);
+	}
+
+}

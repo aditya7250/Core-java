019e7e39bc87ad66296422c226fe7a4d06f796bf
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-06-26 22:29:11 +0530

    Add files via upload

---
+++ Naturalnumberprogram.java
@@ -0,0 +1,13 @@
+package javaprogram26;
+
+public class Naturalnumberprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//      int i = 1;
+      for ( int i= 1; i<=10; i++) {
+    	  System.out.print(i);
+      }
+	}
+
+}

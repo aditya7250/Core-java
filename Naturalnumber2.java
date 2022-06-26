d4b9fa390fcd64fff73bf8ec824b61e3ea92bf72
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-06-26 22:27:34 +0530

    Add files via upload

---
+++ Naturalnumber2.java
@@ -0,0 +1,14 @@
+package javaprogram26;
+
+public class Naturalnumber2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int i,num = 100, sum=0;
+        for (i=1; i<=num; ++i) {
+        	sum = sum+i;
+        }
+         System.out.println("The sum of first 100 natural number is =" +sum);
+	}
+
+}

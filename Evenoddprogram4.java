a12e1a1b02ca7ddaf74875a53aadf1d514f2f455
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-06-26 22:27:18 +0530

    Add files via upload

---
+++ Evenoddprogram4.java
@@ -0,0 +1,14 @@
+package javaprogram26;
+
+public class Evenoddprogram4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int number= 100;
+    System.out.println("Lists of even number form 1 to 100");
+	
+       for (number=1; number<=100; number++) {
+    	   if(number %2==0) {
+    	   System.out.println("Even number is ="+number);
+    	   }}}}
+	

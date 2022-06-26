1fc40449986dc4626208283ba4e0d36b03112432
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-06-26 22:28:09 +0530

    Add files via upload

---
+++ NaturalNumber4.java
@@ -0,0 +1,13 @@
+package javaprogram26;
+
+public class NaturalNumber4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int i;
+    for(i=9; i<=16; i++) {
+    	 System.out.println(i);
+    }
+	}
+
+}

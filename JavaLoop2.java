1508779580c4dbd0ff615b7ef801094335f51e4a
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-06-28 19:26:13 +0530

    Add files via upload

---
+++ JavaLoop2.java
@@ -0,0 +1,15 @@
+package Hackerrank;
+import java.util.*;
+public class JavaLoop2 {
+
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    //     System.out.println("Enter your number");
+        int n = sc.nextInt();
+        int num = n;
+        for (int i = 1; i<11; i++){
+            
+            System.out.println( n +  " x "  +  i+  " = "  +  n*i);
+        }
+    }   
+}

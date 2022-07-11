2310ea2c7a99782f434132245b266379f16140cd
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-07-11 22:27:14 +0530

    Add files via upload

---
+++ Question1.java
@@ -0,0 +1,13 @@
+import javax.crypto.spec.DESedeKeySpec;
+
+/**
+ * Question1
+ */
+public class Question1 {
+
+ public static void main(String[] args) {
+  System.out.println("hello");
+  System.out.println("dfd");
+ }
+
+}

af0f0cd722aae21dbc6fd3b5dfce00dbab070619
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-07-11 22:27:41 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,10 @@
+package com.apnacollege;
+public class main {
+    public static void main [string[]args]
+    {
+     //output
+     system.out.println["*"]  
+     system.out.println["**"]
+     system.out.println["***"]
+    } 
+}

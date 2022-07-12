6ae4cc51bef6fb52fbaf7d9f72aae2028d8ca09c
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-07-12 17:10:55 +0530

    Add files via upload

---
+++ FlotingPointLiterl.java
@@ -0,0 +1,12 @@
+public class FlotingPointLiterl {
+ static public  void main(String[]args){
+double a = 256.7483938;
+double b = 2.567483938e+2;
+System.out.println(a);
+System.out.println(b);
+ double x = 23_737_3773_37.38;
+ System.out.println(x);
+ // double k = _839_39.78_39;//error
+ // double k = 939_339.3_38_;//error
+ }
+}

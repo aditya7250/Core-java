9196443e439ea12f82aa6bc7ecb8dd836d29f08a
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-07-12 17:10:34 +0530

    Add files via upload

---
+++ CharacterLiterl.java
@@ -0,0 +1,10 @@
+public class CharacterLiterl {
+ static public void main(String[]args){
+   char a = 'h';
+   char b = 105;
+   char c = '\u0069';
+   System.out.println(a);
+   System.out.println(b);
+   System.out.println(c);
+ }
+}

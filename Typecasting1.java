0675184b86afc631565c770ba728e5acd8dbb1e2
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-07-13 16:32:09 +0530

    Add files via upload

---
+++ Typecasting1.java
@@ -0,0 +1,12 @@
+public class Typecasting1 {
+ static public void main(String[]args){
+   int a = 10;
+   System.out.println();
+   double b = a;
+   System.out.println(b);
+   double x =  25.3;
+   System.out.println(x);
+   int y = (int) x;
+   System.out.println(y);
+ }
+}

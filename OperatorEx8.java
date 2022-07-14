28ee2a592dd7d952acb5886fe5edb77c0162a34f
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-07-14 23:21:27 +0530

    Add files via upload

---
+++ OperatorEx8.java
@@ -0,0 +1,23 @@
+
+public class OperatorEx8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10,b;
+		//b=a++ - a;
+		//b=++a - a;
+		//b=a++ - a++ -a;
+		//b=++a - a++;
+		//b=--a+a++-a++-a;
+		//b=a++-++a+a--+a++;
+		//b=a++*a--/++a+a--%a++;
+		//b=a+++a;   //b=a++ +a;
+		//b=a++++a;//error
+		//b=a+++++a;//error
+		//b=a+++ ++a;
+		b=a++-++a;
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}

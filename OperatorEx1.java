a8d3bea0a83e2561df04e54d5da0610f8e8351cc
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-07-14 23:19:32 +0530

    Add files via upload

---
+++ OperatorEx1.java
@@ -0,0 +1,14 @@
+
+public class OperatorEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ int a =7/2;
+ System.out.println(a);
+	double b = 7/2;
+	System.out.println(b);
+ double c = 7/2.0;
+   System.out.println(c);
+	}
+
+}

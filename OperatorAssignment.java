1929ddf34579167fe59e737ef520bb47f00a0a04
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-07-14 23:19:12 +0530

    Add files via upload

---
+++ OperatorAssignment.java
@@ -0,0 +1,20 @@
+
+public class OperatorAssignment {
+
+	public static void main(String[] args) {
+				int a=10,b;
+				//b=++a-a+a++;
+				//b=a--+a++-a+a++;
+				//b=--a+a++-a--+a;
+				//b=a++-++a+a--+a--+--a;
+				//b=++a*a--+a--*--a;
+				//b=--a+a++-a;
+				//b=a++-++a-a--;
+				//b=--a*a++/++a-a++-a--;
+               b=--a*a++/a--%a--+--a*a;
+				System.out.println(a);
+				System.out.println(b);
+			}
+		}
+
+

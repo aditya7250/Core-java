7ebb5fe49fa0045ca8da29118f5483fb7505ad70
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-07-16 15:33:42 +0530

    Add files via upload

---
+++ OperatorEx11.java
@@ -0,0 +1,12 @@
+
+public class OperatorEx11 {
+
+	public static void main(String[] args) {
+		int a = 10;
+     	System.out.println(a++);
+		System.out.println(++a);
+		System.out.println(a);
+	}
+	}
+
+

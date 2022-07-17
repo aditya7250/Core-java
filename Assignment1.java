54691f4ec7dfe030cc6e866345f9bb7f45ab8cdb
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-07-17 21:45:56 +0530

    Add files via upload

---
+++ Assignment1.java
@@ -0,0 +1,86 @@
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+//	 int r = 8-9*2/5+6;
+//	 System.out.println(r);
+
+	 //int a = 10; int b=20;
+//	 int r = a*b/2+8*a;
+//	 System.out.println(a);
+//	 System.out.println(b);
+//	 System.out.println(r);
+ 
+//	 int r = a^b;
+//	 System.out.println(a);
+//	 System.out.println(b);
+//	 System.out.println(r);
+//		
+	 
+//	 int r = 4+2*5/10;
+//	 System.out.println(r);
+//	 
+//	 int a = 10;
+//	 int r = a+++3;
+//	 System.out.println(a);
+//	 System.out.println(r);
+//	 
+//	 
+	 
+//		int a = 10;
+//		int r = a*7-6/2+a++;
+//		System.out.println(a);
+//		System.out.println(r);
+		
+//		int a = 10;
+//		int r = 4+ --a + a++;
+//		System.out.println(a);
+//		System.out.println(r);
+		
+//		int a = 10;
+//		int b = 20;
+//		int r = a++ - b++ * a;
+//		System.out.println(r);
+//		System.out.println(a);
+//		System.out.println(b);
+//		
+		
+//		int a = 10; int b = 20;
+//		int r = a++ - b++ *a;
+//		System.out.println(a);
+//		System.out.println(b);
+//		System.out.println(r);
+//	 
+		
+		
+//		int a = 10;
+//		 int b = 20;
+//		 int r =  ++a<b? a--+b:b++/a++;
+//		 System.out.println(a);
+//		 System.out.println(b);
+//		 System.out.println(r);
+		
+		
+//		int a = 10;
+//		int b = 20;
+//		boolean r = a++<--b&&a!=b++;
+//		System.out.println(a);
+//		System.out.println(b);
+//		System.out.println(r);
+		
+		int a = 10;
+		int b = 20;
+		boolean r = a*b++<a++/--b;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(r);
+		
+		
+//		
+		
+		
+	}
+	
+	
+
+}

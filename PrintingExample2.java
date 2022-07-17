b1aeba51e760de0a04e364d0324ca8694d6ce7a7
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-07-18 03:24:25 +0530

    Add files via upload

--- PrintingExample2.java
+++ PrintingExample2.java
@@ -2,11 +2,20 @@
 public class PrintingExample2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println();//allowed
-		//System.out.print();//error
-		//System.out.printf();//error
-		//System.out.format();//error
+		int a=10;
+		double b=23.45;
+		String c="Hello";
+		char d='h';
+		boolean e=true;
+		System.out.printf("%d\n",a);
+		System.out.printf("%f\n",b);
+		System.out.printf("%s\n",c);
+		System.out.printf("%c\n",d);
+		System.out.printf("%b\n",e);
+		
+		System.out.printf("%d\n%f\n%s\n%c\n%b\n",a,b,c,d,e);
+		System.out.printf("%d%n%f%n%s%n%c%n%b%n",a,b,c,d,e);
 	}
-	}
+
+		}
 

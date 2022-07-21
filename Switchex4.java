f56d9b236f36421f926bb4493d1a7f7710f6552e
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-07-21 22:51:56 +0530

    Add files via upload

---
+++ Switchex4.java
@@ -0,0 +1,28 @@
+
+public class Switchex4 {
+
+	public static void main(String[] args) {
+		switch("mohan") {
+//		case 2:
+//			System.out.println("Hi");
+//			break;
+		case "mohan":
+			System.out.println("Hello");
+			break;
+		default:
+			System.out.println("Invalid No.!");
+	}
+	
+	switch("mohan") {
+	case "2":
+		System.out.println("Hi");
+		break;
+	case "mohan":
+		System.out.println("Hello");
+		break;
+	default:
+		System.out.println("Invalid No.!");
+	}
+}
+
+}

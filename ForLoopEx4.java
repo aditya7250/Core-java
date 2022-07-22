5be316f36f0081d0522dc2c6683a2d9738925094
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-07-22 14:21:52 +0530

    Add files via upload

---
+++ ForLoopEx4.java
@@ -0,0 +1,25 @@
+package LoopControl;
+
+public class ForLoopEx4 {
+
+	public static void main(String[] args) {
+		for(int x=1;x<=10;x++) { 
+			System.out.print(x+" ");
+		}
+		System.out.println();
+		int y=10;
+		for(int x=1;x<=y;x++) { 
+			System.out.print(y+" ");
+		}
+		System.out.println();
+		for(int x=1;x<=10;x+=2) { 
+			System.out.print(x+" ");
+		}
+		System.out.println();
+		int sum=0;
+		for(int x=1;x<=10;x++) { 
+			sum += x;
+		}
+		System.out.println("Addition: "+sum);
+	}
+}

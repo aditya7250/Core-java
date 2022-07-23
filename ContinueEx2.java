b80991d2ecf764626d73a05d045f8731ba112f53
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-07-23 17:28:15 +0530

    Add files via upload

---
+++ ContinueEx2.java
@@ -0,0 +1,18 @@
+package LoopControl;
+
+public class ContinueEx2 {
+
+	public static void main(String[] args) {
+		int x=1;
+		while(x<=5){ 
+			System.out.println("Hello");
+			x++;
+			if(x==3) {
+				continue;
+			}
+			System.out.println("Rahul Chauhan");
+			
+		}
+	}
+
+}

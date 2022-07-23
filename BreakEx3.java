076c6217c09770ce01e16e923e4a1eed985455be
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-07-23 17:27:46 +0530

    Add files via upload

---
+++ BreakEx3.java
@@ -0,0 +1,17 @@
+package LoopControl;
+
+public class BreakEx3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=1;
+		do{ 
+			System.out.println("Hello");
+			if(x==3) {
+				break;
+			}
+			System.out.println("Rahul Chauhan");
+			x++;
+		}while(x<=5);
+	}
+}

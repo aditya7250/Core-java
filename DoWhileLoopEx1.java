e068a5b6921c8d22c131195579dc34b8de6667b2
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-07-23 17:28:44 +0530

    Add files via upload

---
+++ DoWhileLoopEx1.java
@@ -0,0 +1,16 @@
+package LoopControl;
+
+public class DoWhileLoopEx1 {
+
+	public static void main(String[] args) {
+java.util.Scanner sc=new java.util. Scanner( System.in );
+		
+		System.out.println("Enter a no.: ");
+		int a=sc.nextInt();
+		int x=1;
+		do{ 
+			System.out.print("Aditya Yadav");
+			x++;
+		}while(x<=a) ;
+	}
+}

8b0f693952a422b8e58b2e19f0f206cd279d0cfc
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-07-24 17:34:07 +0530

    Add files via upload

---
+++ oddeven.java
@@ -0,0 +1,16 @@
+package AssignmentProgram2;
+
+public class oddeven {
+
+	public static void main(String[] args) {
+		java.util.Scanner sc=new java.util. Scanner( System.in );
+		System.out.println("Enter Any Number:");
+		int n = sc.nextInt();
+		if (n%2==0) {
+			System.out.println("Even number");
+		}else {
+    System.out.println("Odd Number");			 
+		}
+	}
+	}
+	

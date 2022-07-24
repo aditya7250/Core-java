84fd5c2cfd10fb81cdbbf1a1ba83a8ae34d3dfec
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-07-24 17:36:27 +0530

    Add files via upload

---
+++ FibonacciProgram.java
@@ -0,0 +1,16 @@
+package AssignmentProgram3;
+
+public class FibonacciProgram {
+
+	public static void main(String[] args) {
+		int n, a = 0, b = 0, c = 1;
+        System.out.print("Fibonacci Series:");
+        for(int i = 1; i <= 100; i++)
+        {
+            a = b;
+            b = c;
+            c = a + b;
+            System.out.print(a+" ");
+        }
+	}
+}

ad8898ac19ed0f0f15d17e02cdd808c7bf8cef5a
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-07-24 17:31:39 +0530

    Add files via upload

---
+++ PatternEx2.java
@@ -0,0 +1,17 @@
+package PatternProgram;
+
+public class PatternEx2 {
+
+	public static void main(String[] args) {
+		int a=4;
+		int b=1;
+		for(int x=1;x<=a;x++) {
+			for(int y=1;y<=b;y++) {
+				System.out.print("*");
+			}
+			System.out.println();
+			b++;
+		}
+	}
+
+}

787840cbd019a87bb73259208191e8dd202fb0cf
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-07-25 15:41:38 +0530

    Add files via upload

---
+++ FunctionEx4.java
@@ -0,0 +1,13 @@
+
+public class FunctionEx4 {
+// This Program is based on Argument With Return Type
+		static int add(int a,int b) {
+			int r=a+b;
+			return r;
+		}
+		public static void main(String[] args) {
+			int r=add(15,48);
+			System.out.println("Sum: "+r);
+		}
+	}
+

1828ea002813cb98d7cab804d1e9a01f79ff9b1e
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-07-25 15:39:10 +0530

    Add files via upload

---
+++ FunctionEx.java
@@ -0,0 +1,26 @@
+
+public class FunctionEx {
+	//public static void main(String[] args) {
+		static void show() {
+		System.out.println("Yoyo Gupta");
+		System.out.println("Gogo Singh");
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Main");
+		display(); //Function Calling/Invocation
+		System.out.println("OK1");
+		System.out.println("OK2");
+		show(); //Function Calling/Invocation 
+		display(); //Function Calling/Invocation 
+	}
+
+	static void display() { //Function Declaration
+		System.out.println("Hello Incapp");
+		System.out.println("Hi INDIA");
+		System.out.println("Rahul Chauhan");
+	}
+}
+
+
+

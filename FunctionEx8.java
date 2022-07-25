96b03eac8ba47a6af351e36bed1f9d676e219c0c
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-07-25 15:42:35 +0530

    Add files via upload

---
+++ FunctionEx8.java
@@ -0,0 +1,13 @@
+
+public class FunctionEx8 {
+		static void show() { 
+			System.out.println("Yoyo Gupta");
+			//show();
+			System.out.println("Gogo Singh");
+		}
+		public static void main(String[] args) {
+			System.out.println("Hi Main");
+			show();
+			System.out.println("Bye Main");
+		}
+	}

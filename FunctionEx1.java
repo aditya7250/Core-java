31e4d0351a4ceeb01c9085b2adfdeb3ee31958c0
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-07-25 15:39:26 +0530

    Add files via upload

---
+++ FunctionEx1.java
@@ -0,0 +1,15 @@
+
+public class FunctionEx1 {
+	// This Program is Based on No Argument No Return Type
+		static void add() {
+			int a=10, b=20;
+			int r=a+b;
+			System.out.println("Sum: "+r);
+		}
+		public static void main(String[] args) {
+			add();
+			//add(4,8);//error
+			add();
+	}
+
+}

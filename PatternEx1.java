55b0e4b90b4174630531fe7c54759be82e9a6597
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-07-25 14:43:15 +0530

    Add files via upload

--- PatternEx1.java
+++ PatternEx1.java
@@ -3,15 +3,13 @@ package PatternProgram;
 public class PatternEx1 {
 
 	public static void main(String[] args) {
-		int a=3;
-	    int b=4;
-		for(int x=1;x<=a;x++) {
-		for(int y=1;y<=b;y++) {
-		
-				System.out.print("*");
-			}
-			System.out.println();
-		}
+		int a = 3;
+		int b = 4;
+		for (int x =1 ; x<=a; x++) {
+			for (int y = 1; y<=b; y++) {
+		System.out.print("*");
 	}
-
+			System.out.println( );
+	}
+}
 }

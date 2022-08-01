e467619591d630765ac375ade583e737f9b85797
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-08-01 23:15:31 +0530

    Add files via upload

---
+++ NotParametrisedConstructor.java
@@ -0,0 +1,25 @@
+package Constructor;
+class Person{
+	String name;
+	int age;
+	void show() {
+		System.out.println(name+" "+age);
+	}
+	Person(){
+		System.out.println("Hello Person");
+		name="Ram";
+		age=10;
+	}
+}
+public class NotParametrisedConstructor {
+
+	public static void main(String[] args) {
+		Person p1=new Person();
+		Person p2=new Person();
+		Person p3=new Person();
+		//p1.Person();//error
+		p1.show();
+		p2.show();
+		p3.show();
+	}
+}

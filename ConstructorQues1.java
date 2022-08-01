072c9f6ee499688794dd255e8d23ff081b2beca2
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-08-01 23:15:16 +0530

    Add files via upload

---
+++ ConstructorQues1.java
@@ -0,0 +1,24 @@
+package Constructor;
+		class P{
+			String x;
+			static int y;
+			void show() {
+				System.out.println(x+" "+y);
+			}
+			P(){
+				System.out.println("Hello P "+x+y);
+				x="Ram";
+				y++;
+			}
+		}    
+		public class ConstructorQues1 {
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+				P p1=new P();
+				P p2=new P();
+				P p3=new P();
+				p1.show();
+				p2.show();
+				p3.show();
+			}
+		}

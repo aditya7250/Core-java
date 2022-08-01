57ab7d7787cf29cd5da2bdec5015a92c184a6be6
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-08-01 23:14:40 +0530

    Add files via upload

---
+++ Constructor1.java
@@ -0,0 +1,18 @@
+package Assignment7;
+		class A{
+			int x=10;
+			String y="Rahul";
+			A() {
+			x=12;
+			System.out.println("Hi");
+			}
+			}
+		public class Constructor1 {
+
+			public static void main(String[] args) {
+			A a = new A();
+			System.out.println(a.x);
+			System.out.println(a.y);
+	}
+
+}

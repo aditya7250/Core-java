771fd3df834455111ea6fb147b0a560cb3c2da6e
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-08-03 02:07:12 +0530

    Add files via upload

---
+++ InheritanceWithConstructorEx3.java
@@ -0,0 +1,26 @@
+package Constructor;
+class M {
+	int a=10;
+	M(int x) { 
+		this();
+		a=x;
+		System.out.println("Hello M");
+	}
+	M() {
+		System.out.println("Hi M");
+	}
+}
+class N extends M {
+	int b=20;
+	N() {
+		super(5);
+		System.out.println("Hi N");
+	}
+}
+public class InheritanceWithConstructorEx3 {
+
+	public static void main(String[] args) {
+		N n=new N();
+		System.out.println(n.a+" "+n.b);
+	}
+}

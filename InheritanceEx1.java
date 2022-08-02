f012ffd0b3e01f147ecff66691210d882f75bc79
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-08-02 17:10:18 +0530

    Add files via upload

--- InheritanceEx1.java
+++ InheritanceEx1.java
@@ -1,26 +1,41 @@
-package Assignment9;
-class A{
-int x;
-A() {
-System.out.println("Hi A");
-x=10;
-}
+package Inheritance;
+class A {
+	int x=10;
+	static int z=10;
+	private int g=60;
+	void showG() {
+		System.out.println(g);
+	}
+	void m1() {
+		System.out.println("Hello A");
+	}
 }
 class B extends A{
-int y;
-B() {
-System.out.println("Hello B");
-x=15;
-}
-void show(){
-System.out.println(x);
-System.out.println(y);
-}
+	int y=20;
+	void m2() {
+		System.out.println("Hello B");
+		System.out.println(x);
+		//System.out.println(g);//error
+		showG();
+	}
 }
 public class InheritanceEx1 {
 
 	public static void main(String[] args) {
-B b= new B();
-b.show();
-}
+		A a=new A();
+		a.x=15;
+		a.z=15;
+		System.out.println(a.x);//15
+		System.out.println(a.z);//15
+		a.m1();
+		B b=new B();
+		System.out.println(b.x);//10
+		System.out.println(b.z);//15
+		System.out.println(b.y);//20 
+		//System.out.println(b.g);//error
+		b.showG();
+		b.m1();
+		b.m2();
+		
+	}
 }

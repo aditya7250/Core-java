76b184a8c1bd9633b33e25fd51e4466f0f9ac222
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-09-03 02:24:03 +0530

    Add files via upload

---
+++ StringEx1.java
@@ -0,0 +1,14 @@
+package String;
+
+public class StringEx1 {
+
+	public static void main(String[] args) {
+		String s1 = "java";
+		char ch []= {'j','a','v','a'};
+		String s2 = new String (ch);
+		String s3 = new String ("java");
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+	}
+}

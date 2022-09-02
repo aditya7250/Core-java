af46ca12cc56e7cc1fd8b3df602477bc0cda2a85
Aaditya kumar yadav <94703172+aditya7250@users.noreply.github.com>
2022-09-03 02:24:18 +0530

    Add files via upload

---
+++ StringEx2.java
@@ -0,0 +1,17 @@
+package String;
+
+public class StringEx2 {
+
+	public static void main(String[] args) {
+				char n[]= {'R','a','h','u','l'};
+				System.out.println(n);
+				//System.out.println(n.toUpperCase());//error
+				System.out.println(n[2]);
+				
+				String s=new String("Rahul");
+				System.out.println(s);
+				System.out.println(s.toUpperCase());
+				//System.out.println(s[2]);//error
+				System.out.println(s.charAt(2));
+			}
+		}
